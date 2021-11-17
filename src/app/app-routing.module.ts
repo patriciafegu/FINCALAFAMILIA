dcdda7bd56302c3b35e0d1a7efcb60d0060a2b39
patriciafegu <patricia.ferrufino@unah.hn>
2021-11-17 14:49:41 -0600

    agregado PersonaComponent en app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TipoProductoComponent } from './modulo-inventario/tipo-producto/tipo-producto.component';
+import {PersonaComponent} from './modulo-persona/persona/persona.component';
 
 const routes: Routes = [
   //{ path:'', redirectTo:'/inicio', pathMatch:'full'},
-  { path: 'tipo', component: TipoProductoComponent }
+  { path: 'tipo', component: TipoProductoComponent },
+  { path: 'peo', component: PersonaComponent }
 ];
 
 @NgModule({
